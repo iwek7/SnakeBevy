400d06723772abae705db60d83ebe55c8c524084
iwek7 <miwanczuk7@gmail.com>
2025-01-12 02:07:01 +0100

    lounch games based on command line arg

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 mod cars;
-mod systems;
 mod sneko;
+mod systems;
 
 use bevy::ecs::query::{QueryData, QueryFilter, WorldQuery};
 
 use std::cmp::PartialEq;
+use std::env;
 use std::ops::Div;
 
 use bevy::prelude::*;
 use rand::Rng;
+
+// example command `cargo run sneko`
 fn main() {
-    // sneko::lounch_snake();
-    cars::launch_cars();
+    let args: Vec<String> = env::args().collect();
+    let game_name = &args[1];
+    println!("Lounching game {}", game_name);
+    match game_name.as_str() {
+        "sneko" => sneko::lounch_snake(),
+        "cars" => cars::launch_cars(),
+        _ => {
+            println!("This game does not exist");
+        }
+    }
+    if game_name == "sneko" {
+        sneko::lounch_snake();
+    } else {
+    }
+    //
+    // cars::launch_cars();
 }
